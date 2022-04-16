296420f53dd07df730acfc4c98a56f13c01c2936
Kevin Wither <kvn.wither@gmail.com>
2022-04-16 04:01:10 -0600

    commit tree 81633caa3159b2ee64fc2d53ea14b4508d5d2205

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+// this is just the dodger
+const dodger = document.getElementById("dodger");
+//code left and right under here
+
+//left dodger
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 5}px`;
+    }
+  }
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+//right dodger
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 5}px`;
+    }
+  }
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  
